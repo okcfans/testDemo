d4f6df83299c36a26ae134d65d01e2a00bb49ef3
zjw <jianwei.zhuang@hand-china.com>
2019-01-16 10:33:44 +0800

    搭建开发框架

---
+++ src/pages/login/login.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { LoadingService } from '../../scripts/service/loading.service';
+import { TabsPage } from '../tabs/tabs';
+
+/**
+ * Generated class for the LoginPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-login',
+  templateUrl: 'login.html',
+})
+export class LoginPage {
+
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    private loading: LoadingService
+  ) {
+    this.loading.hideLoading();
+  }
+
+  login() {
+    this.navCtrl.push(TabsPage);
+  }
+
+}
